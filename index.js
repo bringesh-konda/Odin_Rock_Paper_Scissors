e40954d50d7a325d3d234df0bb5e88f97f9591d3
bringesh-konda <119878875+bringesh-konda@users.noreply.github.com>
2025-10-07 00:07:25 +0530

    Add getHumanChoice function
    This will enable the humans to enter their choice

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ let humanScore = 0;
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
-    if(choice == 0) {
+    if(choice === 0) {
         return 'rock';
     }
-    else if(choice == 1) {
+    else if(choice === 1) {
+        return 'paper';
+    }
+    else {
+        return 'scissors';
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Enter your choice\n 0: Rock\n 1: Paper\n 2: Scissors");
+    if(choice === 0) {
+        return 'rock';
+    }
+    else if(choice === 1) {
         return 'paper';
     }
     else {
