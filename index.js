fd8165e657dcc73f0ad41565a108b497533d0b91
bringesh-konda <119878875+bringesh-konda@users.noreply.github.com>
2025-10-07 00:28:00 +0530

    Add Play Round and play Game
    this will complete the functionality part

--- index.js
+++ index.js
@@ -26,3 +26,56 @@ function getHumanChoice() {
         return 'scissors';
     }
 }
+
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+    console.log(`humanChoice: ${humanChoice}, computerChoice: ${computerChoice}`);
+    if(humanChoice === 'rock') {
+        if(computerChoice === 'scissors') {
+            humanScore++;
+        }
+        else if(computerChoice === 'paper') {
+            computerScore++;
+        }
+    }
+    else if(humanChoice === 'paper') {
+        if(computerChoice === 'rock') {
+            humanScore++;
+        }
+        else if(computerChoice === 'scissors') {
+            computerScore++;
+        }
+    }
+    else if(humanChoice === 'scissors') {
+        if(computerChoice === 'paper') {
+            humanScore++;
+        }
+        else if(computerChoice === 'rock') {
+            computerScore++;
+        }
+    }
+}
+
+function playGame() {
+    let noOfRounds = 5;
+    for(let i = 0; i < noOfRounds; i++) {
+        playRound();
+        console.log('humanScore: ', humanScore);
+        console.log('computerScore: ', computerScore);
+        console.log("");
+    }
+
+    if(computerScore > humanScore) {
+        console.log('Computer Wins');
+    }
+    else if(humanScore > computerScore) {
+        console.log('Hurray... You Win!!!!');
+    }
+    else {
+        console.log('It is a tie');
+    }
+}
+
+playGame();
